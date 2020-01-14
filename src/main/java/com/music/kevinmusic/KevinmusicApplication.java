0ca079011430be3dd088e5dfef7ce02c302785ce
minkunnchan <minkunnchan@yomabank.com>
2020-01-14 16:16:46 +0630

    first commit

---
+++ src/main/java/com/music/kevinmusic/KevinmusicApplication.java
@@ -0,0 +1,13 @@
+package com.music.kevinmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KevinmusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KevinmusicApplication.class, args);
+	}
+
+}
